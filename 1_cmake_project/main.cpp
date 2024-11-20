8fa557c7aabf4dc45f29b8e2b292fb9acfcc60fd
mochenya <2827075398@qq.com>
2024-11-20 18:09:01 +0800

    update code

---
+++ 1_cmake_project/main.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+#include "thread"
+
+void hello()
+{
+    std::cout << "Hello World!" << std::endl;
+}
+
+int main()
+{
+    std::thread t1(hello);
+    t1.join();
+    return 0;
+}
